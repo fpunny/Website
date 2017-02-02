0b3bcdc29bdfd5834f74f5e8b80d109daed06af9
Frederic Pun <frederic.pun@gmail.com>
2017-02-02 10:48:02 -0500

    Revert
    Fked up

--- javascript.js
+++ javascript.js
@@ -1,11 +1,9 @@
 function menuOpen() {
     var x = document.getElementById('nav');
-    x.style.backgroundColor = '#32322C';
     x.style.top = '0px';
 }
 
 function menuClose() {
     var x = document.getElementById('nav');
-    x.style.backgroundColor = '#13C4A3';
-    x.style.top ='-35px';
+    x.style.top ='-38px';
 }
