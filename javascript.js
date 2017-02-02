1a9c4f3bcdcdbcc1bac2bc2df53708d239d1cbda
Frederic Pun <frederic.pun@gmail.com>
2017-02-02 10:44:44 -0500

    Nope
    Nope

--- javascript.js
+++ javascript.js
@@ -1,9 +1,11 @@
 function menuOpen() {
     var x = document.getElementById('nav');
+    x.style.backgroundColor = '#32322C';
     x.style.top = '0px';
 }
 
 function menuClose() {
     var x = document.getElementById('nav');
-    x.style.top ='-38px';
+    x.style.backgroundColor = '#13C4A3';
+    x.style.top ='-35px';
 }
