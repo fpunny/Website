d3bdf89067d40d7b0b8d7df993252507dbcbc13c
Frederic Pun <frederic.pun@gmail.com>
2017-02-02 02:34:58 -0500

    new commit

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function menuOpen() {
+    var x = document.getElementById('nav');
+    x.style.top = '0px';
+}
+
+function menuClose() {
+    var x = document.getElementById('nav');
+    x.style.top ='-38px';
+}
